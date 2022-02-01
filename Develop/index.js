0a1cec52c967f23c04c735e69b62558e15da8623
dani <photomassacre@gmail.com>
2022-02-01 14:12:57 -0800

    Added further functionality to generateMarkDown and worked within the to-dos

--- Develop/index.js
+++ Develop/index.js
@@ -3,185 +3,153 @@
 //including the packages needed to run the application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
+const genMark = require('./utils/generateMarkdown');
 //this is the destination of where the files will be written to
-const { writeFile } = require('./utils/generateMarkdown');
 
 
-const welcomeMessage = () => {
-    console.log(`
-=======================================
-Welcome to Create a professional README
-=======================================
-`);
-};
-//invoking the welcome message
-welcomeMessage();
-
-//initial function for creating the initial questions
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'githubname',
-            message: 'What is your GitHub username? (Required)',
-            validate: githubnameInput => {
-                if (githubnameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'useremail',
-            message: 'What is a good email for you? (Required)',
-            validate: userEmailInput => {
-                if (userEmailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'repotitle',
-            message: 'What is the name of your repo? (Required)',
-            validate: repoTitleInput => {
-                if (repoTitleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your repo title!!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'desc',
-            message: 'Enter project description? (Required)',
-            validate: descInput => {
-                if (descInput) {
-                    return true;
-                } else {
-                    console.log('Please enter project description!!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'installDesc',
-            message: 'What dependencies do you need to install for your application? (Required)',
-            validate: installDescInput => {
-                if (installDescInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the need for any dependencies?');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usageInfo',
-            message: 'How will your application be used? (Required)',
-            validate: usageInfoInput => {
-                if (usageInfoInput) {
-                    return true;
-                } else {
-                    console.log('Please enter usage?');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'contribGuide',
-            message: 'What are the contribution guidelines? (Required)',
-            validate: contribGuideInput => {
-                if (contribGuideInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your contribution guidelines?');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'testInstruct',
-            message: 'How can you test the app? (Required)',
-            validate: testInstructInput => {
-                if (testInstructInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a way to test your app?');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'checkbox',
-            name: 'typeOfLic',
-            message: 'Choose a type of license? (Check all that apply)',
-            choices: ['GPL', 'MIT', 'LGPL', 'Apache License 2.0', 'BSD']
-        },
-
-        //write
-    ]).then(function (answers) {
-        const queryUrl = `https://api.github.com/users/${answers.githubname}/repos?per_page=50`;
-        const shieldUrl = `https://img.shields.io/github/issues/${answers.githubname}/${answers.repotitle}`;
-    })
-};
-
-
-
-
-
-
-//welcoming the using to the get to  know section
-// const getToKnowMessage = () => {
-//     console.log(`
-// =================================
-// Let's Enter Your Personal Details
-// =================================
-// `);
-// };
-
-// getToKnowMessage();
-
-promptUser();
-
-
-//creating the array of 'questions' if there is not one
-// if (!questionData.questions) {
-//     questionData.questions = [];
-// } return inquirer.prompt([
-//     {
-//         type: 'checkbox',
-//         name: 'typeOfLic',
-//         message: 'Choose a type of license? (Check all that apply)',
-//         choices: ['Public Domain', 'Permissive', 'LGPL', 'Copyleft', 'Proprietary'],
-//     }
-// ]);
-
-
-
-
-// console.log('Hello there: confirming that node is running!');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//the destination for the user's questions
+const questions = [
+
+    {
+        type: 'input',
+        name: 'githubname',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubnameInput => {
+            if (githubnameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'useremail',
+        message: 'What is a good email for you? (Required)',
+        validate: userEmailInput => {
+            if (userEmailInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your repo? (Required)',
+        validate: repoInput => {
+            if (repoInput) {
+                return true;
+            } else {
+                console.log('Please enter your repo title!!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: 'Enter project description? (Required)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please enter project description!!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installDesc',
+        message: 'What dependencies do you need to install for your application? (Required)',
+        validate: installDescInput => {
+            if (installDescInput) {
+                return true;
+            } else {
+                console.log('Please enter the need for any dependencies?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will your application be used? (Required)',
+        validate: usageInfoInput => {
+            if (usageInfoInput) {
+                return true;
+            } else {
+                console.log('Please enter usage?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contrib',
+        message: 'What are the contribution guidelines? (Required)',
+        validate: contribGuideInput => {
+            if (contribGuideInput) {
+                return true;
+            } else {
+                console.log('Please enter your contribution guidelines?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How can you test the app? (Required)',
+        validate: testInstructInput => {
+            if (testInstructInput) {
+                return true;
+            } else {
+                console.log('Please enter a way to test your app?');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a type of license? (Check all that apply)',
+        choices: ['GPL', 'MIT', 'LGPL', 'Apache License 2.0', 'BSD', 'None']
+    }
+
+];
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    //look at the current directory and create a file
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
 
-// TODO: Create a function to initialize app
-function init() { }
+// // TODO: Create a function to initialize app
+function init() {
+    console.log(`
+    =======================================
+    Welcome to Create a professional README
+    =======================================
+     `);
+    //this is calling my question array, which is holding my questions
+    inquirer.prompt(questions)
+        .then((response) => {
 
-// Function call to initialize app
+            //you are reference this file and passing in data
+            writeToFile('README.md', genMark(response));
+
+        });
+}
+
+// // Function call to initialize app
 init(); 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+This project is licensed under the ${license} license.`;
+  }
+  return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+# Description
+
+${data.desc}
+
+# Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)  
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+# Installation
+
+${renderLicenseLink(data.license)}
+${renderLicenseSection(data.license)}
+
+---
+
+# Usage
+
+${data.usage}
+
+# Contributing
+
+${data.contrib}
+
+# Tests
+
+${data.test}
+
+
+# Questions
+If you have any additional questions or concerns, I can be reached at danielchristopherallen@gmail.com 
 
 `;
 }
+
 
 module.exports = generateMarkdown;
